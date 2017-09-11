9c7996792521759326fb43120ed396ebb8383d47
unknown <markoconnor1@gmail.com>
2017-09-11 20:40:14 +0100

    Added RSS feed

---
+++ web_assign-master/AssignmentTemplate/SampleTemplate/Controllers/RSSController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace SampleTemplate.Controllers
+{
+    public class RSSController : Controller
+    {
+        public ActionResult Index()
+        {
+            string url = "http://feeds.ign.com/ign/pc-all?format=xml";
+            XmlDocument xml = new XmlDocument();
+            xml.Load(url);
+            XmlNode rootNode = xml.DocumentElement;
+            XmlNode channel = rootNode.FirstChild;
+            XmlNodeList list = channel.ChildNodes;
+            ViewBag.Title = channel.FirstChild.InnerText;
+            ViewBag.List = list;
+            return PartialView("/Views/RSS/Index.cshtml", list);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
