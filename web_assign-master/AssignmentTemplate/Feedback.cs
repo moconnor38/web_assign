77300034d29cb217ef70d8444c83a46c022f7513
isitaboutabicycle <31797364+isitaboutabicycle@users.noreply.github.com>
2017-09-15 21:41:44 +0100

    added Contact Us controller and model with some entries and edited that view

---
+++ web_assign-master/AssignmentTemplate/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleTemplate.Models
+{
+    public class Feedback
+    {
+        private DateTime TimeSubmitted { get; set; }
+        private string Content { get; set; }
+        private bool Read { get; set; }
+    }
+}
--- web_assign-master/AssignmentTemplate/SampleTemplate/Controllers/ContactController.cs
+++ web_assign-master/AssignmentTemplate/SampleTemplate/Controllers/ContactController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SampleTemplate.Models;
 
 namespace SampleTemplate.Controllers
 {
     public class ContactController : Controller
     {
-        // GET: Contact
+        DAO dao = new DAO();
         public ActionResult Index()
         {
-            return View();
+            List<Feedback> feedbackList = new List<Feedback>()
+            {
+                new Feedback{ TimeSubmitted = DateTime.Now, Content = "Nothing to see here!", Read = false },
+                new Feedback { TimeSubmitted = (DateTime.Now), Content = "This is a mad long comment altogether like it goes on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on!", Read = true }
+            };
+
+            return View(feedbackList);
         }
     }
 }
