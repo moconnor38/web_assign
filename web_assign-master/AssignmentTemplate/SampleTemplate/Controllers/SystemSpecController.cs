f5a78830f60b73baf485bc691ce400270b1f7e17
John <10358323@mydbs.ie>
2017-09-15 20:04:13 +0100

    Added XML db and view/model/controller, still need to alter

---
+++ web_assign-master/AssignmentTemplate/SampleTemplate/Controllers/SystemSpecController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Data;
+using SampleTemplate.Models;
+
+namespace SampleTemplate.Controllers
+{
+    public class SystemSpecController : Controller
+    {
+        // GET: SystemSpec
+        public ActionResult Index()
+        {
+            List<Systemreqs> reqs = new List<Systemreqs>();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Server.MapPath(@"~/App_Data/specs.xml"));
+
+            foreach (XmlNode node in xmlDoc.SelectNodes("/Spec/game"))
+            {
+                reqs.Add(new Systemreqs
+                {
+                    Title = node["Title"].InnerText,
+                    Processor = node["Processor"].InnerText,
+                    Memory = node["Memory"].InnerText,
+                    Storage = node["Storage"].InnerText,
+                    Video = node["Video"].InnerText
+                });
+            }
+            return View(reqs);
+        }
+    }
+}
---
+++ web_assign-master/AssignmentTemplate/SampleTemplate/Models/Systemreqs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleTemplate.Models
+{
+    public class Systemreqs
+    {
+        public string Title { get; set; }
+        public string Processor { get; set; }
+        public string Memory { get; set; }
+        public string Storage { get; set; }
+        public string Video { get; set; }
+
+    }
+}
