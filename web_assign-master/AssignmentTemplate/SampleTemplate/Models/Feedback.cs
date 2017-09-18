8bdf23a5d9c36fde60aaaa7d7b888d8a55f23b18
isitaboutabicycle <31797364+isitaboutabicycle@users.noreply.github.com>
2017-09-18 17:19:52 +0100

    added dummy fields in the Contact form

--- web_assign-master/AssignmentTemplate/SampleTemplate/Models/Feedback.cs
+++ web_assign-master/AssignmentTemplate/SampleTemplate/Models/Feedback.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SampleTemplate.Models
 {
     public class Feedback
     {
+        [Display(Name = "Time Submitted")]
+        [Required]
+        [RegularExpression("[A-Za-z]+")]
         public DateTime TimeSubmitted { get; set; }
+
+        [Display(Name = "Content")]
+        [Required]
+        [RegularExpression("[A-Za-z]+")]
         public string Content { get; set; }
+
+        [Display(Name = "Read")]
+        [Required]
+        [RegularExpression("[A-Za-z]+")]
         public bool Read { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
     }
 }
