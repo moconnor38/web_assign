e31796f87c656ccc59a49b5fb53a1a1307a68d5d
isitaboutabicycle <31797364+isitaboutabicycle@users.noreply.github.com>
2017-09-18 14:48:25 +0100

    Merged with ContactUs branch

---
+++ web_assign-master/AssignmentTemplate/SampleTemplate/Models/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleTemplate.Models
+{
+    public class Feedback
+    {
+        public DateTime TimeSubmitted { get; set; }
+        public string Content { get; set; }
+        public bool Read { get; set; }
+    }
+}
